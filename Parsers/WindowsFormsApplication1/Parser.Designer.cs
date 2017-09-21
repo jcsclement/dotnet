c0cf32feb3fc10d2da0eaa5a34cb0cbc6914d403
Clement <jcsclement@gmail.com>
2017-09-21 09:56:26 +0530

    Changed XML label

--- Parsers/WindowsFormsApplication1/Parser.Designer.cs
+++ Parsers/WindowsFormsApplication1/Parser.Designer.cs
@@ -83,9 +83,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(9, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(154, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "XML text area";
+            this.label2.Text = "XML text area (revert a commit)";
             // 
             // lblRuntime
             // 
