12e3450eaa2a875748dfc61796007e269200cd86
Clement <jcsclement@gmail.com>
2017-09-26 13:47:18 +0530

    Adding SHA project

---
+++ SecureHash/SecureHashing/SecureHashing/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace SecureHashing
+{
+    public partial class fromSHA : Form
+    {
+        public fromSHA()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            txtEncrypted.Text = GetMD5HashData(txtPlainText.Text);
+        }
+
+        private string GetMD5HashData(string data) 
+        {
+            MD5 md5 = MD5.Create();
+            byte[] hashData = md5.ComputeHash(Encoding.Default.GetBytes(data));
+            StringBuilder returnValue = new StringBuilder();
+            for (int i = 0; i < hashData.Length; i++)
+            {
+                returnValue.Append(hashData[i].ToString());
+            }
+            return returnValue.ToString();
+        }
+
+
+        private bool ValidateMD5HashData(string inputData, string storedHashData)
+        {
+            //hash input text and save it string variable
+            string getHashInputData = GetMD5HashData(inputData);
+
+            if (string.Compare(getHashInputData, storedHashData) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void fromSHA_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(ValidateMD5HashData(txtPlainText.Text, txtEncrypted.Text).ToString());
+        }
+    }
+}
